d10b116a4af0c7e2a6b80a026f15059afdc51b17
shengbozhou1981 <52717783+shengbozhou1981@users.noreply.github.com>
2023-09-10 11:05:43 -0400

    Update handel-register-user-request.py

--- .github/scripts/handel-register-user-request.py
+++ .github/scripts/handel-register-user-request.py
@@ -34,9 +34,9 @@ for username, email in user_info:
 with open(file_path, 'w') as file:
     yaml.dump(data, file)
 
-# 创建 Pull Request
-pr_title = f"Add authorized user(s) from Issue #{issue_number}"
-pr_body = f"This PR adds authorized user(s) from Issue #{issue_number}."
-pr = repo.create_pull(title=pr_title, body=pr_body, base="master", head=f"pull-request-branch-{issue_number}")
+# # 创建 Pull Request
+# pr_title = f"Add authorized user(s) from Issue #{issue_number}"
+# pr_body = f"This PR adds authorized user(s) from Issue #{issue_number}."
+# pr = repo.create_pull(title=pr_title, body=pr_body, base="master", head=f"pull-request-branch-{issue_number}")
 
 print(f"Pull Request created: {pr.html_url}")
